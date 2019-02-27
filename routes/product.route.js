29b77498090dc9887a3fb5e7557335b996fa3c34
Austin Webb <stevenaustinwebb@gmail.com>
2019-02-26 23:55:29 -0500

    added CORS

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router();
-var cors = require('cors')
 
-// Require the controllers WHICH WE DID NOT CREATE YET!!
 const product_controller = require('../controllers/product.controller');
-
-// a simple test url to check that all of our files are communicating correctly.
 
 router.post('/create', product_controller.product_create);
 
